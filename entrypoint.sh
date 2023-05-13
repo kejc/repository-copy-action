764b31a07aef5dd704bd7b1e3c5c443adc4545c9
Kenneth Clausen <me@kjc.dk>
2023-05-13 18:39:49 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,10 @@ git clone $2 /dest --depth=1
 #git commit -am "action[repository-copy-action] $1 -> $2"
 #git push
 
+echo "****** FILES ******" >&2
+echo ">> source" >&2
 ls /source
+echo ">> dest" >&2
 ls /dest
 
 # FIXME: Change the ssh key for project after done
