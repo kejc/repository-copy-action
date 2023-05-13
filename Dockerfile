5db341352117ed6049dd16dfbfb090db5860319e
Kenneth Clausen <me@kjc.dk>
2023-05-13 16:22:02 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM alpine:3.18
-RUN apk update && \
-    apk add git
+RUN apk --update add git openssh-client
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 VOLUME /app
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 
 mkdir -p /app ~/.ssh
 
-echo "$1" >&2
-echo "$2" >&2
-
-echo $1 > ~/.ssh/id_rsa
-
-cat ~/.ssh/id_rsa
+echo $3 > ~/.ssh/id_rsa
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
 
 #git clone $1 /app --depth=1
+
+# FIXME: remove
+echo "$1" >&2
+echo "$2" >&2
