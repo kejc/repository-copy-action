12d44d5e35ab35be973d36cd57ab012508af164b
Kenneth Clausen <me@kjc.dk>
2023-05-13 17:31:28 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ mkdir -p /app ~/.ssh
 
 echo "****** SSH SETUP ******" >&2
 eval "$(ssh-agent -s)"
-ssh-keyscan -H github.com >< ~/.ssh/known_hosts
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 echo "$3" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
