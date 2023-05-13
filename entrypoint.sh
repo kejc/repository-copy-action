402b624f2a6a5f3aaa5289eb9a3eaaa2c6def5f4
Kenneth Clausen <me@kjc.dk>
2023-05-13 18:04:28 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,7 @@ chmod 600 ~/.ssh/id_rsa
 echo "****** SSH SETUP ******" >&2
 eval "$(ssh-agent -s)"
 
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 
 ssh-add ~/.ssh/id_rsa
 
