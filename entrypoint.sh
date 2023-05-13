1c18770f6117a6c6eb2c821287e59b7a66d19787
Kenneth Clausen <me@kjc.dk>
2023-05-13 18:38:24 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ echo "$3" > /root/.ssh/id_rsa
 ssh-add /root/.ssh/id_rsa
 git clone $1 /source --depth=1
 
-#echo "****** DESTINATION REPO HANDLE ******" >&2
-#echo "$4" > /root/.ssh/id_rsa
-#git clone $2 /dest --depth=1
-#
+echo "****** DESTINATION REPO HANDLE ******" >&2
+echo "$4" > /root/.ssh/id_rsa
+git clone $2 /dest --depth=1
+
 #echo "****** COPY FILES ******" >&2
 #if [ -f "/source/.gitignore" ]; then
 #  cp /source/.gitignore /dest/
@@ -20,4 +20,9 @@ git clone $1 /source --depth=1
 #git commit -am "action[repository-copy-action] $1 -> $2"
 #git push
 
+ls /source
+ls /dest
+
 # FIXME: Change the ssh key for project after done
+
+
