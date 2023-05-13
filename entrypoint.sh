f0234a9bb8433a4b1ec8a44c3ddf69d4e3ccc292
Kenneth Clausen <me@kjc.dk>
2023-05-13 16:26:22 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,11 @@ chmod 0600 ~/.ssh/id_rsa
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
+echo $3 >&2
+
 #git clone $1 /app --depth=1
 
+# FIXME: Change the ssh key for project after done
 # FIXME: remove
 echo "$1" >&2
 echo "$2" >&2
