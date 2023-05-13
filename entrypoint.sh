7161d1477e9b11717965592e270b92c28d5e4b63
Kenneth Clausen <me@kjc.dk>
2023-05-13 17:15:52 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,9 @@ echo "$3" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 
-#git clone $1 /app --depth=1
+git clone $1 /app --depth=1
+
+ls -lah /app
 
 # FIXME: Change the ssh key for project after done
 # FIXME: remove
