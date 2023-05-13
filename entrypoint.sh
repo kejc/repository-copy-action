92201497a2df4bd31642c06d9049b8fe94317b98
Kenneth Clausen <me@kjc.dk>
2023-05-13 17:54:55 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 echo "****** SETUP FILES & FOLDERS ******" >&2
 mkdir -p /app ~/.ssh
 touch ~/.ssh/known_hosts
+touch ~/.ssh/id_rsa
+
+echo "****** SET PERMISSIONS ******" >&2
 echo "$3" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
@@ -10,9 +13,9 @@ echo "****** SSH SETUP ******" >&2
 eval "$(ssh-agent -s)"
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 ssh-keygen -R github.com
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+ssh-keygen -R github.com
 ssh-add ~/.ssh/id_rsa
-
-wc -l ~/.ssh/id_rsa
 
 echo "****** GIT SOURCE DATA ******" >&2
 git clone $1 /app --depth=1
@@ -20,6 +23,3 @@ git clone $1 /app --depth=1
 ls -lah /app
 
 # FIXME: Change the ssh key for project after done
-# FIXME: remove
-echo "$1" >&2
-echo "$2" >&2
